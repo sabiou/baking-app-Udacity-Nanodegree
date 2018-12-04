7aa1009f8511cad6b96f81d1e732154f9d8e230d
Farouk <sfarouk298@gmail.com>
2018-12-04 07:16:24 +0100

    added StepsViewHolder.java

---
+++ app/src/main/java/xyz/godi/bakingapp/holders/StepsViewHolder.java
@@ -0,0 +1,10 @@
+package xyz.godi.bakingapp.holders;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class StepsViewHolder extends RecyclerView.ViewHolder {
+    public StepsViewHolder(View itemView) {
+        super(itemView);
+    }
+}
