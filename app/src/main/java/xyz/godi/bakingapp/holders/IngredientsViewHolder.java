3b9e9fa58e31000a34be34a9c27d7bf9cda23c26
Farouk <sfarouk298@gmail.com>
2018-12-04 07:15:50 +0100

    added IngredientsViewHolder.java

---
+++ app/src/main/java/xyz/godi/bakingapp/holders/IngredientsViewHolder.java
@@ -0,0 +1,11 @@
+package xyz.godi.bakingapp.holders;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class IngredientsViewHolder extends RecyclerView.ViewHolder {
+
+    public IngredientsViewHolder(View itemView) {
+        super(itemView);
+    }
+}
